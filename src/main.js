a504d908dc84339158f458cad977d7870c055cd6
xing_bean <3181828241@qq.com>
2020-06-29 18:03:03 +0800

    feat(forum):新增技术论坛功能

--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from './router'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import './styles/index.scss'
+import router from "./router";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import "./styles/index.scss";
 
 Vue.use(ElementUI);
 //标题
 router.beforeEach((to, from, next) => {
-  if(to.meta.title){
-    document.title = to.meta.title
+  if (to.meta.title) {
+    document.title = to.meta.title;
   }
-  next()
-})
-
+  next();
+});
 
 Vue.config.productionTip = false;
 
--- src/views/forum/list.vue
+++ src/views/forum/list.vue
@@ -3,15 +3,20 @@
     <el-container>
       <el-header class="header bg-white">
         <el-row class="row just-start-align-center">
-          <el-col :span="4">
-            <div class="align">
-              <img src="../../assets/logo.png" alt="logo">
+          <el-col :span="4" class="row just-align-center">
+            <div class="logo">
+              <img src="../../assets/logo.png" alt="logo" />
             </div>
           </el-col>
           <el-col :span="16">
             <div>
               <el-tabs>
-                <el-tab-pane v-for="item in navList" :key="item.name" :label="item.label" :name="item.name" />
+                <el-tab-pane
+                  v-for="item in navList"
+                  v-bind:key="item.name"
+                  :label="item.label"
+                  :name="item.name"
+                />
               </el-tabs>
             </div>
           </el-col>
@@ -29,12 +34,64 @@
         <el-row>
           <el-col :span="2" class="bg-white mg-r-20">
             <ul class="con-left">
-              <li v-for="item in classify" :key="item.id">
-                {{item.name}}
+              <li v-for="item in classify" v-bind:key="item.id">
+                {{ item.name }}
               </li>
             </ul>
           </el-col>
-          <el-col :span="18">2</el-col>
+          <el-col :span="18" class="con-center bg-white">
+            <ul>
+              <li v-for="item in classifyList" v-bind:key="item.id">
+                <div class="top row just-between-align-center pd-20">
+                  <h2 class="title">
+                    <el-link class="txt">{{ item.title }}</el-link>
+                  </h2>
+                  <div class="font-14 color-3">
+                    <span class="pd-r-20">
+                      <i class="el-icon-view"></i>
+                      <span class="pd-l-5">{{ item.views }}</span>
+                    </span>
+                    <span class="pd-r-20">
+                      <i class="el-icon-chat-dot-square"></i>
+                      <span class="pd-l-5">{{ item.comments }}</span>
+                    </span>
+                  </div>
+                </div>
+                <p class="center">
+                  {{ item.contents.substring(0, 150) }}
+                  <span v-if="item.contents.length > 150">...</span>
+                </p>
+                <div class="bottom row just-between-align-center">
+                  <div class="row just-start-align-center">
+                    <div class="row just-start-align-center authorInfo">
+                      <div class="author">
+                        <el-image
+                          class="img"
+                          :src="item.author.img"
+                          :fit="fit"
+                        />
+                      </div>
+                      <div class="name font-14 pd-lr-10">
+                        {{ item.author.name }}
+                      </div>
+                    </div>
+                    <div class="time font-12">
+                      <span>发布时间：</span>
+                      <span>{{ item.author.time }}</span>
+                    </div>
+                  </div>
+                  <ul class="pd-r-20 row just-end">
+                    <li class="pd-l-10" v-for="tag in item.type" :key="tag">
+                      <el-tag size="mini" type="info" effect="dark">{{
+                        tag
+                      }}</el-tag>
+                    </li>
+                  </ul>
+                </div>
+                <el-divider />
+              </li>
+            </ul>
+          </el-col>
           <el-col :span="3">3</el-col>
         </el-row>
       </el-main>
@@ -42,87 +99,121 @@
   </div>
 </template>
 <script>
-  export default{
-    name:'forumList',
-    data(){
-      return{
-        navList:[
-          {
-            label:'技术论坛',
-            name:'forum'
+import img from "../../assets/author.jpeg";
+export default {
+  name: "forumList",
+  data() {
+    return {
+      navList: [
+        {
+          label: "技术论坛",
+          name: "forum"
+        },
+        {
+          label: "模型知识",
+          name: "model"
+        },
+        {
+          label: "专家信息",
+          name: "expert"
+        },
+        {
+          label: "开发者指南",
+          name: "guide"
+        },
+        {
+          label: "个人中心",
+          name: "personal"
+        }
+      ],
+      classify: [
+        {
+          id: "1",
+          name: "移动开发"
+        },
+        {
+          id: "2",
+          name: "前端开发"
+        },
+        {
+          id: "3",
+          name: "服务端开发/管理"
+        },
+        {
+          id: "4",
+          name: "游戏开发"
+        },
+        {
+          id: "5",
+          name: "编程语言"
+        },
+        {
+          id: "6",
+          name: "数据库"
+        },
+        {
+          id: "7",
+          name: "企业开发"
+        },
+        {
+          id: "8",
+          name: "图像/多媒体"
+        },
+        {
+          id: "9",
+          name: "系统运维"
+        },
+        {
+          id: "10",
+          name: "软件工程"
+        },
+        {
+          id: "11",
+          name: "开源硬件"
+        },
+        {
+          id: "12",
+          name: "物联网"
+        },
+        {
+          id: "13",
+          name: "其他类型"
+        }
+      ],
+      classifyList: [
+        {
+          id: "1",
+          title:
+            "学编程这么久，还傻傻分不清什么是方法（method），什么是函数学编程这么久，还傻傻分不清什么是方法（method），什么是函数学编程这么久，还傻傻分不清什么是方法（method），什么是函数学编程这么久，还傻傻分不清什么是方法（method），什么是函数",
+          views: "1231",
+          comments: "321",
+          contents:
+            "学编程这么久，还傻傻分不清什么是方法（method），什么是函数学编程这么久，还傻傻分不清什么是方法（method），什么是函数学编程这么久，还傻傻分不清什么是方法（method），什么是函数学编程这么久，还傻傻分不清什么是方法（method），什么是函数学编程这么久，还傻傻分不清什么是方法（method），什么是函数学编程这么久，还傻傻分不清什么是方法（method），什么是函数学编程这么久，还傻傻分不清什么是方法（method），什",
+          author: {
+            img: img,
+            name: "王艳",
+            time: "2020-06-29 15:58"
           },
-          {
-            label:'模型知识',
-            name:'model'
+          type: ["移动开发", "物联网", "开源硬件", "软件工程"]
+        },
+        {
+          id: "2",
+          title:
+            "2学编程这么久，还傻傻分不清什么是方法（method），什么是函数学编程这么久，还傻傻分不清什么是方法（method），什么是函数学编程这么久，还傻傻分不清什么是方法（method），什么是函数学编程这么久，还傻傻分不清什么是方法（method），什么是函数",
+          views: "1231",
+          comments: "321",
+          contents:
+            "2学编程这么久，还傻傻分不清什么是方法（method），什么是函数学编程这么久，还傻傻分不清什么是方法（method），什么是函数学编程这么久，还傻傻分不清什么是方法（method），什么是函数学编程这么久，还傻傻分不清什么是方法（method），什么是函数",
+          author: {
+            img: img,
+            name: "王艳2",
+            time: "2020-06-29 15:58"
           },
-          {
-            label:'专家信息',
-            name:'expert'
-          },
-          {
-            label:'开发者指南',
-            name:'guide'
-          },
-          {
-            label:'个人中心',
-            name:'personal'
-          }
-        ],
-        classify:[
-          {
-            id:'1',
-            name:'移动开发'
-          },
-          {
-            id:'2',
-            name:'前端开发'
-          },
-          {
-            id:'3',
-            name:'服务端开发/管理'
-          },
-          {
-            id:'4',
-            name:'游戏开发'
-          },
-          {
-            id:'5',
-            name:'编程语言'
-          },
-          {
-            id:'6',
-            name:'数据库'
-          },
-          {
-            id:'7',
-            name:'企业开发'
-          },
-          {
-            id:'8',
-            name:'图像/多媒体'
-          },
-          {
-            id:'9',
-            name:'系统运维'
-          },
-          {
-            id:'10',
-            name:'软件工程'
-          },
-          {
-            id:'11',
-            name:'开源硬件'
-          },
-          {
-            id:'12',
-            name:'物联网'
-          },
-          {
-            id:'13',
-            name:'其他类型'
-          }
-        ]
-      }
-    }
+          type: ["移动开发", "物联网", "开源硬件", "软件工程"]
+        }
+      ],
+      fit: "fill"
+    };
   }
+};
 </script>
